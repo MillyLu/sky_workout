d4241eb905655fa1ef5ce319a5d23b00c9f42585
Roman <aromawriter@mail.ru>
2023-04-05 12:51:24 +0300

    feature/mainPage_3.0

--- src/components/WorkoutItem/WorkoutItem.jsx
+++ src/components/WorkoutItem/WorkoutItem.jsx
@@ -1,10 +1,11 @@
-import s from './WorkoutItem.module.css'
+import s from "./WorkoutItem.module.css";
 
 export const WorkoutItem = (props) => {
-    return (
-      <div className={s.item}>
-        <img className={s.img} src={props.img} alt="img" />
-        <p className={s.title}>{props.name}</p>
-      </div>
-    );
-}
+  return (
+    <div className={s.item}>
+      <img className={s.img} src={props.img} alt="img" />
+      <p className={s.title}>{props.name}</p>
+      {props.button}
+    </div>
+  );
+};
