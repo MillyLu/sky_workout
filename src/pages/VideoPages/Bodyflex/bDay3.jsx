6705025af8ab6ad4f38af467192328a5083621ff
Arnikikkkkkkkkkkkk <ftnfhfgtm@gmail.com>
2023-04-05 23:50:09 +0500

    full

---
+++ src/pages/VideoPages/Bodyflex/bDay3.jsx
@@ -0,0 +1,19 @@
+import { Video } from "../../../components/VideoPages/video";
+
+export const BDay3 = () =>{
+    return (
+        <Video 
+
+        name={"Бодифлекс"}
+        day={"3"}
+        description={"Тренировка мышц ягодиц"}
+        url={"https://youtu.be/JTCaG3Y9xlM"}
+        exercises1={"• Крендель (15 повторений)"}
+        exercises2={"• Собачка (20 повторений)"}
+        exercises3={"• Кошка (15 повторений)"}
+        progress1={"Крендель "}
+        progress2={"Собачка"}
+        progress3={"Кошка"}
+        />
+    )
+}
