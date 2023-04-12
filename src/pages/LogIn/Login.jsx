0ea9ddcd763e0545a04eef5b473f47094f12e851
Roman <aromawriter@mail.ru>
2023-04-12 15:07:55 +0300

    feature/autorisation

--- src/pages/LogIn/Login.jsx
+++ src/pages/LogIn/Login.jsx
@@ -1,19 +1,47 @@
 import s from "./Login.module.css";
 import { ReactComponent as Logo } from "../../img/logo.svg";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { setUser } from "../../store/Slices/userSlice";
 
-export const LogIn = ({ setSignUp, toogleLogin }) => {
+export const LogIn = ({ setSignUp }) => {
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const dispatch = useDispatch();
+
+  const toogleLogin = (email, password) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken,
+          })
+        );
+      })
+      .catch(()=>alert('Такого пользователя не существует'));
+  };
+
   return (
     <div className={s.login}>
       <Logo alt="logo" />
       <input
         className={`${s.input} ${s.input_login}`}
-        type="text"
-        placeholder="Логин"
+        type="email"
+        value={mail}
+        placeholder="E-mail"
+        onChange={(e) => setMail(e.target.value)}
       />
       <input
         className={`${s.input} ${s.input_password}`}
         type="password"
+        value={pass}
         placeholder="Пароль"
+        onChange={(e) => setPass(e.target.value)}
       />
       <button className={s.button_login} onClick={() => toogleLogin()}>
         Войти
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,26 +1,63 @@
 import s from "./SignUp.module.css";
 import { ReactComponent as Logo } from "../../img/logo.svg";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { setUser } from "../../store/Slices/userSlice";
 
 export const SignUp = () => {
+  const [login, setLogin] = useState("");
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const dispatch = useDispatch();
+
+  const toggleSignUp = (email, password) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken,
+          })
+        );
+      })
+      .catch(console.error);
+  };
   return (
     <div className={s.sign_up}>
       <Logo alt="logo" />
       <input
         className={`${s.input} ${s.input_login}`}
         type="text"
+        value={login}
         placeholder="Логин"
+        onChange={(e) => setLogin(e.target.value)}
+      />
+      <input
+        className={`${s.input} ${s.input_login}`}
+        type="email"
+        value={mail}
+        placeholder="E-mail"
+        onChange={(e) => setMail(e.target.value)}
       />
       <input
         className={`${s.input} ${s.input_password}`}
         type="password"
+        value={pass}
         placeholder="Пароль"
+        onChange={(e) => setPass(e.target.value)}
       />
       <input
         className={`${s.input} ${s.input_password}`}
         type="password"
         placeholder="Повторите пароль"
       />
-      <button className={s.button_registration}>Зарегистрироваться</button>
+      <button className={s.button_registration} onClick={() => toggleSignUp()}>
+        Зарегистрироваться
+      </button>
     </div>
   );
 };
