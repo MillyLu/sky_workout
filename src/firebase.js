86c9d9a58546663e56faa11d534ecbb254e31e74
MillyLu <mji23@mail.ru>
2023-04-13 10:39:13 +0300

    fix auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,19 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyBBTjd9wjQLOFLKg4-FtvF76N-9pVPb2tA',
+  authDomain: 'skypro-workout.firebaseapp.com',
+  databaseURL: 'https://skypro-workout-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'skypro-workout',
+  storageBucket: 'skypro-workout.appspot.com',
+  messagingSenderId: '707634527024',
+  appId: '1:707634527024:web:f14f8ede7833fb69f926f5',
 };
 
-const app = initializeApp(firebaseConfig);
-export default app;
+initializeApp(firebaseConfig);
+
+const db = getDatabase();
+
+export default db;
