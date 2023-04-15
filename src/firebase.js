3994a5a23bf8c34c22d1638ed77e725dc5d42998
MillyLu <mji23@mail.ru>
2023-04-15 22:03:02 +0300

    variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-
+// try use local variables
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
