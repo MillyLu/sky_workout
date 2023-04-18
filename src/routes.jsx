80d0fd48382d0bf2bd4040a89409a282bb7ba0c1
Arnikikkkkkkkkkkkk <ftnfhfgtm@gmail.com>
2023-04-19 01:58:09 +0500

    InputProgress

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,13 +8,6 @@ import { Stretching } from "./pages/Stretching/stretching";
 import { Yoga } from "./pages/Yoga/yoga";
 import { Main } from "./pages/Main/Main";
 import { Profile } from "./pages/Profile/profile";
-import { YDay1 } from "./pages/VideoPages/Yoga/yDay1";
-import { YDay2 } from "./pages/VideoPages/Yoga/yDay2";
-import { YDay3 } from "./pages/VideoPages/Yoga/yDay3";
-import { YDay4 } from "./pages/VideoPages/Yoga/yDay4";
-import { YDay5 } from "./pages/VideoPages/Yoga/yDay5";
-import { SDay1 } from "./pages/VideoPages/Stretching/sDay1";
-import { BDay1 } from "./pages/VideoPages/Bodyflex/bDay1";
 import { Error } from "./pages/Error/Error";
 import Workout from "./pages/Workout";
 
@@ -27,13 +20,6 @@ export const AppRoutes = () => {
       <Route path="/stretching" element={<Stretching />} />
        <Route element={<ProtectedRoute isAllowed={Boolean(isAuth)} />}>
         <Route path="/profile" element={<Profile />} />
-        <Route path="/yDay1" element={<YDay1 />} />
-        <Route path="/yDay2" element={<YDay2 />} />
-        <Route path="/yDay3" element={<YDay3 />} />
-        <Route path="/yDay4" element={<YDay4 />} />
-        <Route path="/yDay5" element={<YDay5 />} />
-        <Route path="/sDay5" element={<SDay1 />} />
-        <Route path="/bDay1" element={<BDay1 />} />
         <Route path="/workout" element={<Workout />} />
         <Route path="/workout/:id" element={<Workout />} />
       </Route>
