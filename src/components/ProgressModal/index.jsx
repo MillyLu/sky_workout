70b20d75d9e494ab4a416e91f323dd023e0868bd
Arnikikkkkkkkkkkkk <ftnfhfgtm@gmail.com>
2023-04-19 01:57:54 +0500

    InputProgress

--- src/components/ProgressModal/index.jsx
+++ src/components/ProgressModal/index.jsx
@@ -1,38 +1,35 @@
-import { useState } from 'react'
 import ButtonMain from '../Exercises/ButtonMain/index'
 import classes from './index.module.css'
-import InputProgress from './InputProgress/index'
+import { useForm } from 'react-hook-form'
+import  InputProgress  from './Progress/index'
+
 
   const ProgressModal = ({ data, onClick }) => {
-    const [exerciseCounts, setExerciseCounts] = useState(
-      Array(data.length).fill(1)
-    );
-  
-    const handleInputChange = (index, event) => {
-      const value = parseInt(event.target.value);
-      const newCounts = [...exerciseCounts];
-      newCounts[index] = isNaN(value) ? 1 : Math.max(1, Math.min(value, data[index][1]));
-      setExerciseCounts(newCounts);
-    };
-  
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm()
+    const onSubmit = (data) => {
+      console.log(data)
+      onClick()
+    }
    
 
     return (
-      <form className={classes.form} >
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
       <h2 className={classes.title}>Мой прогресс</h2>
       <div className={classes.inputs}>
         {data?.exercise?.map((exercise, index) => (
           <div key={index} >
             <label className={classes.text}>
               {`Сколько раз вы сделали упражнение ${exercise[0].toLowerCase()}?`}
-              <input
-              className={classes.input}
-                type="number"
-                value={exerciseCounts[index]}
-                min={1}
-                max={exercise[1]}
-                onChange={(e) => handleInputChange(index, e)}
-              />
+              <InputProgress
+              name={exercise[0]}
+              register={register}
+              errors={errors}
+              max={exercise[1]}
+            />
             </label>
           </div>
         ))}
